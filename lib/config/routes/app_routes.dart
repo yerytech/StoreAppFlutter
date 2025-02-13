29ca0c23d92140a14876807d5b4aafdc85da4ef5
yerytech <yerytecnology@gmail.com>
2025-02-13 10:15:46 -0400

    Refactor route transitions to use animation.drive for improved performance

--- lib/config/routes/app_routes.dart
+++ lib/config/routes/app_routes.dart
@@ -13,30 +13,30 @@ final GoRouter appRouter= GoRouter(
    routes: <RouteBase>[
     GoRoute(path: "/e",pageBuilder: (context,state){
       return CustomTransitionPage(child: const LoadingScreen(), transitionsBuilder: (context,animation,secundaryAnimation,child){
-        return FadeTransition(opacity: CurveTween(curve: Curves.ease).animate(animation),child: child,);
+        return FadeTransition(opacity: animation.drive(CurveTween(curve: Curves.ease)),child: child,);
       });
     }),
     GoRoute(path: "/", pageBuilder: (context, state) {
-      return CustomTransitionPage(child: const RegisterScreen(), transitionsBuilder: (context,animaton,secundaryAnimation,child){
-        return FadeTransition(opacity: CurveTween(curve: Curves.ease).animate(animaton),child: child,);
+      return CustomTransitionPage(child: const LoginScreen(), transitionsBuilder: (context,animation,secundaryAnimation,child){
+        return FadeTransition(opacity: animation.drive(CurveTween(curve: Curves.ease)),child: child,);
       });
     },),
     GoRoute(
       path: "/register",
       pageBuilder: (context, state) {
         return CustomTransitionPage(child: const RegisterScreen(), transitionsBuilder: (context,animation,secundaryAnimation,child){
-          return FadeTransition(opacity: CurveTween(curve: Curves.easeIn).animate(animation),child: child,);
+          return FadeTransition(opacity:animation.drive(CurveTween(curve: Curves.ease)),child: child,);
         });
       },
   ),
     GoRoute(path: "/home",pageBuilder: (context, state) {
       return CustomTransitionPage(child: const HomeScreen(), transitionsBuilder: (context,animation,secundaryAnimation,child){
-        return FadeTransition(opacity: CurveTween(curve: Curves.ease).animate(animation),child: child,);
+        return FadeTransition(opacity: animation.drive(CurveTween(curve: Curves.ease)),child: child,);
       });
     },),
     GoRoute(path: "/product",pageBuilder: (context,state){
       return  CustomTransitionPage(child: const ProductScreen(), transitionsBuilder: (context,animation,secundaryAnimate,child){
-        return FadeTransition(opacity: CurveTween(curve: Curves.ease).animate(animation),child: child,);
+        return FadeTransition(opacity: animation.drive(CurveTween(curve: Curves.ease)),child: child,);
       });
     }),
    ],
