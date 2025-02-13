57a97310fc19e885473ec5f7471d220aeab60be6
yerytech <yerytecnology@gmail.com>
2025-02-13 11:42:19 -0400

    Add .env to .gitignore and improve error handling in ApiStore

--- lib/config/api/api_store.dart
+++ lib/config/api/api_store.dart
@@ -8,7 +8,7 @@ Future<Response> get(String path)async{
   try {
     return await _dio.get("localhost:3000/api/$path");
   } catch (e) {
-    throw Exception("Error al optenerdata");
+    throw Exception("Error al optener data $e");
   }
 }
 
