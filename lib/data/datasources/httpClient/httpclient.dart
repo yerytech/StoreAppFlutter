ebcc89502a7b0e9680c5d60d79c44ec9ed22b834
yerytech <yerytecnology@gmail.com>
2025-02-16 21:29:45 -0400

    Refactor HTTP client methods to consolidate error handling in a single request method

--- lib/data/datasources/httpClient/httpclient.dart
+++ lib/data/datasources/httpClient/httpclient.dart
@@ -7,78 +7,43 @@ class CustomHttpclient implements CustomHttpClientRepository {
   final String _baseUrl;
 CustomHttpclient(this._baseUrl, {required Dio httpClient}):_httpClient=httpClient;
 
+Future<Response>_request(Future<Response>request)async{
+  try {
+    final response= await request;
+    if (response.data>=200 && response.statusCode! <300){
+      return response.data;     
+    }else{
+      throw Exception("Error en la respuesta ${response.statusCode}");
+    }
+  } catch (e) {
+     throw Exception("Error al realizar la solicitud $e");
+  }
+}
+
+
   @override
   Future<Response> delete(String path, String id) async {
-    try {
-       final response= await _httpClient.delete("$_baseUrl/$path/$id");
-        if(response.statusCode! >= 200 && response.statusCode! < 300){
-          return response.data;
-        }else{
-          throw Exception("Error al borrar Elemento con el id: ${response.statusCode}");
-        }
-    } catch (e) {
-      throw Exception("Error al borrar Elemento con el id: $id");
-    }
-    
+   return _request(_httpClient.delete("$_baseUrl/$path/$id"));  
   }
 
   @override
   Future<Response> get(String path) async{
-    
-    try {
-      final response =  await _httpClient.get("$_baseUrl$path");
-        if (response.statusCode! >= 200 && response.statusCode! < 300) {
-          return response.data;
-        }else{
-          throw Exception("Error al hacer el get ${response.statusCode}");
-        }
-    } catch (e) {
-      throw Exception("Error al obtener dato $e");
-    }
+    return _request(_httpClient.get("$_baseUrl/$path"));
   }
 
   @override
   Future<Response> patch(String path, String id) async {
-      try {
-         final response = await _httpClient.patch("$_baseUrl/$path/$id");
-        if (response.statusCode! >= 200 && response.statusCode! < 300) {
-           return response.data;
-        }else{
-          throw Exception("Error al realizar patch ${response.statusCode}");
-        }
-      } catch (e) {
-        throw Exception("Error al realizar patch $e");
-      }
+    return _request(_httpClient.patch("$_baseUrl/$path/$id"));
   }
 
   @override
   Future<Response> post(String path,dynamic data) async {
-    
-  try {
-    final response =  await _httpClient.post("$_baseUrl$path",data: data);
-    if (response.statusCode! >= 200 && response.statusCode! < 300) {
-      return response.data;
-    }else{
-       throw Exception("Error al realizar el post ${response.statusCode}");
-    }
-  } catch (e) {
-    throw Exception("Error al realizar el post $e");
-  }}
+     return _request(_httpClient.post("$_baseUrl/$path/",data: data));
+  }
 
   @override
   Future<Response> put(String path, dynamic data) async{
-   
-    try {
-      final response = await  _httpClient.put("$_baseUrl$path",data: data);
-      if (response.statusCode! >= 200 && response.statusCode! < 300) {
-        return response.data;
-        
-      }else{
-        throw Exception("Error al realizar el put ${response.statusCode}");
-      }
-    } catch (e) {
-      throw Exception("Error al realizar el put $e");
-    }
+     return _request(_httpClient.put("$_baseUrl/$path",data: data));
   }
   
 }
