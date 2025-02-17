fe6711ce7986fbba01e93cdc646c4d71692c4628
yerytech <yerytecnology@gmail.com>
2025-02-17 16:43:53 -0400

    Refactor SecureStorageUseCase methods for improved readability; add SecureStorageNotifier for state management

--- lib/features/domain/usecases/auth/secure_storage_use_case.dart
+++ lib/features/domain/usecases/auth/secure_storage_use_case.dart
@@ -3,16 +3,21 @@ import 'package:productsapp/features/domain/repositories/securestorage/secure_st
 class SecureStorageUseCase  {
   final SecureStorageRepository _secureStorageRepository;
   SecureStorageUseCase(this._secureStorageRepository);
-  
-   Future <bool>save (String key ,String value) async{
-    return await _secureStorageRepository.save(key: key, value: value);
-   }
+    
 
-  Future<String>read(String key)async{
-    return await _secureStorageRepository.read(key: key);
+    // Guardar token
+   Future <bool>save (String key ,String value) {
+    return  _secureStorageRepository.save(key: key, value: value);
+   }
+   
+  //  Obtener token
+  Future<String>read(String key){
+    return  _secureStorageRepository.read(key: key);
   }
-  Future<bool> delete(String key)async{
-    return await _secureStorageRepository.delete(key: key);
+
+  // Borrar token 
+  Future<bool> delete(String key){
+    return  _secureStorageRepository.delete(key: key);
   }
 
 }
