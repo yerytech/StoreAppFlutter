d2710ab63ba4637d5fcf682f58cf63079ebc268f
yerytech <yerytecnology@gmail.com>
2025-02-17 13:35:16 -0400

    Implement SecureStorageRepository to manage secure storage operations

---
+++ lib/features/data/repository/secure_storage_repository.dart
@@ -0,0 +1,26 @@
+import 'package:productsapp/features/data/datasources/tokenStorage/tokenstore.dart';
+import 'package:productsapp/features/domain/repositories/securestorage/secure_store.dart';
+
+class SecureStorageRepositoryImpl implements SecureStorageRepository {
+  final TokenStorage _tokenStorage;
+  
+  SecureStorageRepositoryImpl({ required TokenStorage tokenStorage }):_tokenStorage=tokenStorage;
+  
+  @override
+  Future<bool> delete({required String key}) {
+   
+    return _tokenStorage.delete(key: key);
+  }
+  
+  @override
+  Future<String> read({required String key}) {
+     return _tokenStorage.read(key: key);
+  }
+  
+  @override
+  Future<bool> save({required String key, required String value}) {
+     return _tokenStorage.save(key: key, value: value);
+  
+  }
+  
+}
